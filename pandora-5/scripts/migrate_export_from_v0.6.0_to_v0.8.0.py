25cee52e88f479ba8867df18d7f7cd60761f5a2f
Willem Mouton <willemgmouton@gmail.com>
2022-06-23 14:07:20 +0200

    added pandora-5 genesis config.

---
+++ pandora-5/scripts/migrate_export_from_v0.6.0_to_v0.8.0.py
@@ -0,0 +1,25 @@
+import json
+
+in_file = 'exported.json'
+out_file = 'genesis.json'
+
+# Load genesis
+with open(in_file, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Update chain ID
+data['chain_id'] = 'pandora-5'
+
+# Update genesis time
+data['genesis_time'] = '2022-07-01T12:00:00Z'
+
+# Update initial height
+data['initial_height'] = '1'
+
+# Finishing touches (replace & with unicode)
+data = json.dumps(data, indent=2, sort_keys=True, ensure_ascii=False)
+data = data.replace('&', '\\u0026')
+
+# Output migrated genesis
+with open(out_file, 'w', encoding='utf-8') as f:
+    f.write(data)
